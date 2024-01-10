c33e4d242f2abc5cffb4b3e76d7f063fcfa9f1a8
jnlongnecker <jlnecker1@gmail.com>
2024-01-10 17:27:11 -0500

    Comments

--- index.js
+++ index.js
@@ -180,9 +180,13 @@ let penStrikes = false;
 
 /**************** MAIN ****************/
 
+// Run normal simulation
 runSimulation();
+
 data = {};
 penStrikes = true;
+
+// Run simulation with penetrating strikes
 runSimulation();
 
 /**************** FUNCTIONS ****************/
@@ -191,24 +195,31 @@ async function runSimulation() {
     for (let strMod of strengthModifiers) {
         for (let enemy of enemies) {
 
+            // Set up data storage
             if (!data[enemy.name]) {
                 data[enemy.name] = {};
             }
-
             let modKey = "atMod" + strMod;
             if (!data[enemy.name][modKey]) {
                 data[enemy.name][modKey] = {};
             }
+
+            // Run combat simulation once per iteration
             for (let i = 0; i < simulationIterations; i++) {
+                // Since strength mods do not affect vibro weapons, they end up getting simulated more often
                 runVibroSimulation(enemy);
                 runBladeSimulation(enemy, strMod);
             }
         }
     }
 
+    // Calculate the averages
     calculateAverages();
+
+    // Calculate the weapon comparison
     calculateVibroStats();
 
+    // Write to file
     if (penStrikes) {
         fs.writeFile('data_ps.json', JSON.stringify(data, null, 4), err => {
             if (err) {
@@ -224,9 +235,12 @@ async function runSimulation() {
     }
 }
 
+// Calculates and writes the comparison of vibro to long blade
 function calculateVibroStats() {
     let vibroStats = {};
     for (let enemy in data) {
+
+        // Build data arrays of vibro weapons and normal weapons
         let vw = [];
         let blades = [];
         for (let weapon in data[enemy]) {
@@ -240,30 +254,44 @@ function calculateVibroStats() {
                 vw.push(newData);
             }
         }
+
+        // Do the comparison for this enemy
         for (let vibWep of vw) {
+
+            // Calculate the actual comparison
             let bladeComparison = {};
             for (let modData of blades) {
                 for (let weapon in modData) {
                     if (weapon == "name") continue;
+
+                    // Base case is that it never beats the weapon
                     if (!bladeComparison[weapon]) {
                         bladeComparison[weapon] = "Never";
                     }
+
+                    // If the weapon has a lower average hits to kill, mark the strength mod this occurs at
                     if (modData[weapon].averageHitsToKill < vibWep.averageHitsToKill) {
                         if (bladeComparison[weapon] == "Never") {
                             let mod = modData.name.split("atMod")[1];
                             bladeComparison[weapon] = `At str mod ${mod}`;
                         }
+
+                        // If it's the first one (mod 0), the weapon always beats the vibro weapon
                         if (modData.name == "atMod0") {
                             bladeComparison[weapon] = "Always";
                         }
                     }
                 }
             }
+
+            // Ensure data exists
             if (!vibroStats[vibWep.name]) {
                 vibroStats[vibWep.name] = {
                     against: {}
                 }
             }
+
+            // Write the data to the object
             vibroStats[vibWep.name]["against"][enemy] = {
                 "Average Simulated Damage": vibWep.simulatedAverageDamage,
                 "Average Hits to Kill": vibWep.averageHitsToKill,
@@ -273,6 +301,8 @@ function calculateVibroStats() {
             }
         }
     }
+
+    // Write the data to file
     if (penStrikes) {
         fs.writeFile('weapon_comparisons_with_ps.json', JSON.stringify(vibroStats, null, 4), err => {
             if (err) {
@@ -288,9 +318,12 @@ function calculateVibroStats() {
     }
 }
 
+// Calculates some statistical averages for each weapon
 function calculateAverages() {
     for (let enemy in data) {
         for (let weapon in data[enemy]) {
+
+            // The long blade case
             if (weapon.includes("atMod")) {
                 for (let enemyData in data[enemy][weapon]) {
                     let simulationsRun = data[enemy][weapon][enemyData].totalSimulationsRun;
@@ -305,7 +338,9 @@ function calculateAverages() {
                     data[enemy][weapon][enemyData]["averagePenetrationsPerHit"] = totalPens / totalHits;
                     data[enemy][weapon][enemyData]["averageFailedPensPerKill"] = totalFailed / simulationsRun;
                 }
-            } else {
+            }
+            // The vibro weapon case
+            else {
                 let simulationsRun = data[enemy][weapon].totalSimulationsRun;
                 let totalDamage = data[enemy][weapon].totalSimulatedDamage;
                 let totalPens = data[enemy][weapon].totalSimulatedPenetrations;
@@ -322,6 +357,7 @@ function calculateAverages() {
     }
 }
 
+// Runs a complete simulation for all weapons, strength mods and enemies
 function runBladeSimulation(enemy, strMod) {
     for (let weapon of blades) {
 
@@ -383,10 +419,11 @@ function runBladeSimulation(enemy, strMod) {
     }
 }
 
+// Runs a single to-death simulation for all vibro weapons
 function runVibroSimulation(enemy) {
     for (let vibroWeapon of vibroWeapons) {
 
-        // Run kill simulation on this enemy for this weapon
+        // Write average damage; just to compare to the theoretical to statistical to ensure correctness
         let avgDamage = vibroWeapon.damageBonus + vibroWeapon.dieAmount * dieAverage(vibroWeapon.damageDie);
         let hitsToKill = 0;
         let hp = enemy.hp;
@@ -394,22 +431,36 @@ function runVibroSimulation(enemy) {
         let totalDamage = 0;
         let totalPens = 0;
         let totalFailedPens = 0;
+
+        // Run simulation until creature death, tracking total damage dealt, total penetrations and failed penetrations
         while (hp > 0) {
+            // 20 on a d20 is a crit
             let isCrit = Random(1, 20) == 20;
 
             // Long blades get an additional +2 PV on crit on top of the normal +1 PV
+            // Turn on penetration debug stats to see this to be true
             let critBonus = isCrit ? 3 : 0;
+
+            // Vibro weapon penetration calculation is the enemy AV plus any PV bonuses
             let pens = RollDamagePenetrations(enemy.av, enemy.av + critBonus, enemy.av + critBonus);
+
+            // Crits simply add 1 to the penetration count
             if (isCrit) {
                 pens += 1;
             }
+
+            // Penetrating strikes simply adds 1 to the penetration count
             if (penStrikes) {
                 pens += 1;
             }
+
+            // Roll damage
             let damage = vibroWeapon.damageBonus;
             for (let i = 0; i < vibroWeapon.dieAmount; i++) {
                 damage += rollDie(vibroWeapon.damageDie);
             }
+
+            // Apply damage affects
             hp -= damage * pens;
             totalDamage += damage * pens;
             totalPens += pens;
@@ -438,40 +489,72 @@ function runVibroSimulation(enemy) {
     }
 }
 
+// Returns a random die result
 function rollDie(die) {
     return Random(1, die);
 }
 
+// Returns the statistical average for a given die
 function dieAverage(die) {
     return (die * 0.5) + 0.5;
 }
 
+// Recreation of the RollDamagePenetrations() function from Caves of Qud
 function RollDamagePenetrations(TargetInclusive, Bonus, MaxBonus) {
+
+    // Comments are my comments added
+    // Source code is from XRL.Rules.Stat.RollDamagePenetrations()
+
+    // num represents the number of penetrations
     num = 0;
+
+    // num2 represents the number of successful penetrations from last attempt
     num2 = 3;
+
+    // All 3 attempts must penetrate for a subsequent penetration attempt to occur
     while (num2 == 3) {
         num2 = 0;
+
+        // Roll penetration 3 times
         for (i = 0; i < 3; i++) {
+
+            // Roll -1 to 8
             num3 = Random(1, 10) - 2;
             num4 = 0;
+
+            // If the roll was an 8, add 8 and roll again for however many 8's we roll
             while (num3 == 8) {
                 num4 += 8;
                 num3 = Random(1, 10) - 2;
             }
+
+            // num4 holds our cumulative dice roll total
             num4 += num3;
+
+            // num5 holds the dice roll + the minimum of the maximum penetration bonus vs the creatures actual penetration bonus
             num5 = num4 + ((Bonus > MaxBonus) ? MaxBonus : Bonus);
+
+            // If our roll + bonus exceeds the enemy AV, it's a penetration
             if (num5 > TargetInclusive) {
                 num2++;
             }
         }
+
+        // If a single roll penetrated, that's a successful penetration. Add it to the return value
         if (num2 >= 1) {
             num++;
         }
+
+        // Reduce our actual penetration bonus by 2
+        // Note this is not our capped bonus
         Bonus -= 2;
     }
+
+    // Return the number of penetrations
     return num;
 }
 
+// Recreation of the Random() function from Caves of Qud
 function Random(min, max) {
     return Math.round(Math.random() * (max - min)) + min;
 }
